25ad15d5a605d07e6c451c3581f75cbb9f7d01f9
Kristín Jörundsdóttir <kristin@Kristins-MacBook-Pro.local>
2018-09-04 14:18:26 +0000

    Algorithm written for max_int

--- max_int.py
+++ max_int.py
@@ -3,8 +3,10 @@
 #3 Þegar kerfið hættir að keyra að það prenti út 
 
 num_int = int(input("Input a number: "))
-
-
-
+max_int = 0
+while num_int >= 0:
+  num_int = int(input("Input a number: "))
+  if num_int > max_int :
+     max_int = num_int
 
 print("The maximum is", max_int)
