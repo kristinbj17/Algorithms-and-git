8824d34b04b393f1353b74845956af52e110ac10
Kristín Jörundsdóttir <kristin@Kristins-MacBook-Pro.local>
2018-09-04 14:10:55 +0000

    Algorithm written for max_int

--- max_int.py
+++ max_int.py
@@ -1,3 +1,7 @@
+#1 Byrja á því að gera while lykkju til að skoða hvort inputið sé tala
+#2 Kerfið hætti að keyra þegar neikvæð tala er slegin inn
+#3 Þegar kerfið hættir að keyra að það prenti út 
+
 num_int = int(input("Input a number: "))
 
 
