6047823e0827cb11b888f707279f9e013da54488
Kristín Jörundsdóttir <kristin@Kristins-MacBook-Pro.local>
2018-09-04 14:57:14 +0000

    asdfg

--- sequence.py
+++ sequence.py
@@ -4,19 +4,19 @@
 
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
 
-n1 = 1
-n2 = 2
-n3 = 3
+a1 = 1
+a2 = 2
+a3 = 3
 sum_int = 0
 
-print (n1)
-print (n2)
-print (n3)
+print (a1)
+print (a2)
+print (a3)
 
 for i in range (n-3):
-  sum_int = n1 + n2 + n3
-n1 = n2
-n2 = n3
-n3 = sum_int
+  sum_int = a1 + a2 + a3
+  print (sum_int)
+  a1 = a2
+  a2 = a3
+  a3 = sum_int
 
-print (sum_int)
