fb1e14c137db620bbfeabeb0e1680d9c4b08e42f
Kristín Jörundsdóttir <kristin@Kristins-MacBook-Pro.local>
2018-09-04 13:59:37 +0000

    Algorithm written for max_int

--- max_int.py
+++ max_int.py
@@ -1,3 +1,6 @@
-num_int = int(input("Input a number: "))    # Do not change this line
-# Fill in the missing code
-print("The maximum is", max_int)    # Do not change this line
+num_int = int(input("Input a number: "))
+
+
+
+
+print("The maximum is", max_int)
